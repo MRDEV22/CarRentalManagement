5f1fe6964d518e2954bc0e8bc2e76cb567e71cc6
mretana <mretana@pinsa.com>
2022-01-12 17:45:20 -0700

    Added REST API Endpoints and Unit of Work pattern

---
+++ CarRentalManagement/Server/Controllers/BrandsController.cs
@@ -0,0 +1,108 @@
+﻿#nullable disable
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarRentalManagement.Shared.Domain;
+using CarRentalManagement.Server.IRepository;
+
+namespace CarRentalManagement.Server.Controllers
+{
+    //[Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public BrandsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: api/Brands
+        [HttpGet]
+        public async Task<IActionResult> GetBrands()
+        {
+            var brands = await _unitOfWork.Brands.GetAll();
+            return Ok(brands);
+        }
+
+        // GET: api/Brands/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBrand(int id)
+        {
+            var brand = await _unitOfWork.Brands.Get(q => q.Id == id);
+
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(brand);
+        }
+
+        // PUT: api/Brands/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBrand(int id, Brand brand)
+        {
+            if (id != brand.Id)
+            {
+                return BadRequest();
+            }
+
+            _unitOfWork.Brands.Update(brand);
+
+            try
+            {
+                await _unitOfWork.Save(HttpContext);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await BrandExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Brands
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Brand>> PostBrand(Brand brand)
+        {
+            await _unitOfWork.Brands.Insert(brand);
+            await _unitOfWork.Save(HttpContext);
+
+            return CreatedAtAction("GetBrand", new { id = brand.Id }, brand);
+        }
+
+        // DELETE: api/Brands/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBrand(int id)
+        {
+
+            var brand = await _unitOfWork.Brands.Get(q => q.Id == id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            await _unitOfWork.Brands.Delete(id);
+            await _unitOfWork.Save(HttpContext);
+
+            return NoContent();
+        }
+
+        private async Task<bool> BrandExists(int id)
+        {
+            var brand = await _unitOfWork.Brands.Get(q => q.Id == id);
+            return brand != null;
+        }
+    }
+}
---
+++ CarRentalManagement/Server/IRepository/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using CarRentalManagement.Shared.Domain;
+
+namespace CarRentalManagement.Server.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task Save(HttpContext httpContext);
+        IGenericRepository<Brand> Brands { get; }
+        IGenericRepository<Booking> Bookings { get; }
+        IGenericRepository<Color> Colors { get; }
+        IGenericRepository<Customer> Customer { get; }
+        IGenericRepository<Model> Models { get; }
+        IGenericRepository<Vehicle> Vehicles { get; }
+    }
+}
